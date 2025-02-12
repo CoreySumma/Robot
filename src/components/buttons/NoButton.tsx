5fa21c393bef39816d83a494455b3571b35c16de
CoreySumma <CoreySumma@gmail.com>
2025-02-12 13:06:03 -0600

    Added no button animation

--- src/components/buttons/NoButton.tsx
+++ src/components/buttons/NoButton.tsx
@@ -1,9 +1,25 @@
+import {useState } from "react";
 import "./button.scss";
 
 export default function NoButton() {
+  const [buttonPosition, setButtonPosition] = useState({
+    left: "30%",
+    bottom: "10%",
+  });
+
+  function snapToNewPosition() {
+    const randomX = Math.floor(Math.random() * window.innerWidth * .8);
+    const randomY = Math.floor(Math.random() * window.innerHeight * .8);
+    setButtonPosition({ left: `${randomX}px`, bottom: `${randomY}px` });
+  }
+
   return (
-    <button className="no_button">
+    <button
+      className="no_button"
+      style={buttonPosition}
+      onMouseEnter={snapToNewPosition}
+    >
       No
     </button>
-  )
+  );
 }

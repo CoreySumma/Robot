5b10e333c05d7563682631a614b28c1322b83b2d
CoreySumma <CoreySumma@gmail.com>
2025-02-12 13:27:17 -0600

    Attached click event to yes button

--- src/components/buttons/YesButton.tsx
+++ src/components/buttons/YesButton.tsx
@@ -1,11 +1,16 @@
 import "./button.scss";
 
+
 export default function YesButton() {
+  const portfolio = "https://coreysumma.github.io/portfolioweb/";
+  const handleClick = () => {
+    window.open(portfolio, "_blank");
+  };
   return (
     <>
-      <input type="checkbox" id="yes-button" />
+      <div id="yes-button" />
       <label htmlFor="yes-button">
-        <div className="yes_button">
+        <div className="yes_button" onClick={handleClick}>
           <span className="yes_text">Yes</span>
           <i className="yes_tick">✓</i>
           {[...Array(52)].map((_, i) => (
